6a39aab13048b3923a8689bbc1a36396e1639883
Josue Caraballo <josue.caraballo@gmail.com>
2020-02-14 18:54:37 -0600

    Create download-data.py

---
+++ py/download-data.py
@@ -0,0 +1,4 @@
+# write code here to accept JSON input of "uri='uri to download'" and prints out the full path to the downloaded file.
+
+# the file should be placed in the `<root>/data/<file here with OG filename>`
+# where <root> is the root of this repo
