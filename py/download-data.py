bea8a0f671683334e417f187417e17315e80eff8
Josue Caraballo <josue.caraballo@gmail.com>
2020-02-14 22:25:01 -0600

    Update download-data.py
    saving initial draft of script

--- py/download-data.py
+++ py/download-data.py
@@ -2,3 +2,25 @@
 
 # the file should be placed in the `<root>/data/<file here with OG filename>`
 # where <root> is the root of this repo
+
+impot os
+import sys
+import json
+import requests
+from pathlib import Path
+path = Path("/here/your/path/file.txt")
+print(path.parent)
+
+if __name__=="__main__":
+  arg = sys.argv[-1]
+  input_dict = json.loads(arg)
+  uri = input_dict["uri"]
+  filename = uri.split("/")[-1]
+  curr = Path(os.getcwd())
+  par = curr.parent
+  data_dir = os.path.join(par, "data")
+  fpath = os.path.join(data_dir, filename)
+  with open(fpath, "wb") as f:
+      r = requests.get(url)
+      f.write(r.content)
+  print(fpath)
