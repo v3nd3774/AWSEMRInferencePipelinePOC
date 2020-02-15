9bbbe37aec43800794d2e5c65fc1ae5f7595d698
Josue Caraballo <josue.caraballo@gmail.com>
2020-02-14 22:28:30 -0600

    Update download-data.py

--- py/download-data.py
+++ py/download-data.py
@@ -8,19 +8,20 @@ import sys
 import json
 import requests
 from pathlib import Path
-path = Path("/here/your/path/file.txt")
-print(path.parent)
 
 if __name__=="__main__":
   arg = sys.argv[-1]
   input_dict = json.loads(arg)
   uri = input_dict["uri"]
   filename = uri.split("/")[-1]
+  
   curr = Path(os.getcwd())
   par = curr.parent
   data_dir = os.path.join(par, "data")
+  
   fpath = os.path.join(data_dir, filename)
   with open(fpath, "wb") as f:
       r = requests.get(url)
       f.write(r.content)
+      
   print(fpath)
